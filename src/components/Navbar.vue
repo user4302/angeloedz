b3aca25efc300b1e21eb8776a7384452304b4a51
user4302 <4ngelo@outlook.com>
2021-10-27 18:11:47 +0530

    added navbar drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,3 +16,44 @@
         >Contact Me<v-icon right>mdi-email</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app color="#7AF6D0">
+      stuff
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<style>
+.nav-bar {
+  margin-left: 19%;
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1%;
+}
+.nav-bar a {
+  font-size: 150%;
+  font-family: "Oswald", sans-serif;
+  color: #ffffff;
+}
+.nav-bar a.router-link-exact-active {
+  color: #42b983;
+}
+.nav-option {
+  flex: 1;
+  text-align: center;
+  text-decoration-line: none;
+}
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
