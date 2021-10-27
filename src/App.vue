b7dd8e72073e7e88ce670ebf24c1cfec0692ea12
user4302 <4ngelo@outlook.com>
2021-10-27 18:09:12 +0530

    added navbar as component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,25 @@
 <template>
   <v-app>
-    <v-main class="main">
-      <!-- <div class="nav-bar">
-        <router-link class="nav-option" to="/resume">Resume</router-link>
-        <router-link class="nav-option" to="/about">About</router-link>
-        <router-link class="nav-option" style="font-size: 300%" to="/">Angelo</router-link>
-        <router-link class="nav-option" to="/skills">Skills</router-link>
-        <router-link class="nav-option" to="/fun">Fun</router-link>
-      </div> -->
-      <router-view />
-    </v-main>
+    <Navbar />
+    <v-content class="main"><router-view /></v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
 export default {
   name: "App",
-
+  components: { Navbar },
   data: () => ({
     //
   }),
 };
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
-
-.main{
-  color: #ffffff;
+<style scoped>
+.main {
   background: #353535;
   text-align: center;
-}
-
-body {
-  background: #353535;
-}
-
-.nav-bar{
-  margin-left: 19%;
-  width: 60%;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  padding: 1%;
-}
-.nav-bar a {
-  font-size: 150%;
-  font-family: 'Oswald', sans-serif;
-  color: #ffffff;
-}
-.nav-bar a.router-link-exact-active {
-  color: #42b983;
-}
-.nav-option {
-  flex: 1;
-  text-align: center;
-  text-decoration-line: none;
+  color: #7af6d0;
 }
 </style>
