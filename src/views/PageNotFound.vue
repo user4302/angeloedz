b5ff354690cdbc1cdaa223ca124dd91c3f6c8097
user4302 <4ngelo@outlook.com>
2021-10-27 20:03:40 +0530

    added functionality to button

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -3,6 +3,8 @@
     <h1>404 - Page Not Found</h1>
     <p>This page no longer exists or was moved to another location.</p>
 
-    <v-btn text flat color="#7AF6D0"><v-icon left>mdi-home</v-icon>Home</v-btn>
+    <v-btn text flat color="#7AF6D0" to="/">
+      <v-icon left>mdi-home</v-icon>Home
+    </v-btn>
   </div>
 </template> 
