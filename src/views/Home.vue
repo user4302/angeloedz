7f52195b34de593d153d8511fff5fda7c29966aa
user4302 <4ngelo@outlook.com>
2021-10-28 21:57:07 +0530

    added bio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,64 +19,67 @@
 
     <div class="bio">
       <h1>Bio</h1>
+      <v-container>
+        <v-row class="bio-table">
+          <v-col class="bio-table-col-1" cols="12" sm="6" md="2">
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >Full Name</v-card
+            >
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >Date of Birth</v-card
+            >
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >Nationality</v-card
+            >
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >Spoken Languages</v-card
+            >
+          </v-col>
+
+          <v-col class="bio-table-col-2" cols="6" md="2">
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >Angelo Eksith De Zoysa</v-card
+            >
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >20 JAN 1997</v-card
+            >
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >Sri Lankan</v-card
+            >
+            <v-card class="pa-2 dark_1" flat tile style="color: #7af6d0"
+              >English, Sinhala</v-card
+            >
+          </v-col>
+          <v-col cols="6" md="8">
+            <v-card
+              class="pa-2 dark_1"
+              flat
+              tile
+              left
+              style="color: #7af6d0; text-align: left"
+            >
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        accusamus, dolore animi perspiciatis, ipsam rem nihil quae hic veritatis
-        dignissimos aperiam adipisci! Eum maxime, ipsum molestiae, vero ducimus
-        eius, nobis obcaecati non iure nostrum inventore. Autem cumque dolorum
-        inventore ut magni vero expedita repellendus alias iure impedit quam,
-        magnam quisquam quasi itaque. Velit ea nobis reprehenderit eius.
-        Repellat eveniet consectetur, harum sed distinctio enim officia, quidem
-        hic aspernatur doloremque repellendus facilis esse nulla voluptatum iste
-        deleniti numquam illo. Iure, cupiditate eligendi inventore, illum
-        repudiandae corporis veritatis dicta assumenda fugit, eos doloribus?
-        Culpa aut, praesentium quisquam accusamus laborum unde? Minima explicabo
-        error quas odit. Quam sunt officiis voluptatem sapiente, velit commodi
-        exercitationem libero nisi dolores similique veniam aspernatur facere
-        quos qui nesciunt soluta! Sequi, nulla architecto, modi dolores,
-        eligendi laboriosam accusamus magnam quaerat minus quos fugiat hic
-        dolorum asperiores. Dolorem nulla blanditiis veritatis, ab, laborum modi
-        labore dolores odio, at optio recusandae sed rem consequatur vitae
-        suscipit sint quae. Illum molestiae, non natus vitae voluptatum
-        explicabo nam veritatis obcaecati? Consequatur aut reiciendis quasi nam
-        debitis sed, ab architecto! Impedit magnam dolores iusto? Deleniti ut
-        asperiores quibusdam eligendi officia necessitatibus totam vel, autem in
-        iusto soluta fuga vitae distinctio natus voluptates culpa corporis
-        facilis accusantium? Explicabo, consequatur. Expedita, doloremque modi
-        cupiditate enim earum sapiente praesentium sunt, quos veniam repellat
-        nam beatae error illum voluptatem voluptatibus. Exercitationem fugiat in
-        earum perferendis tempore, quia officia magni, a laboriosam, fugit
-        cumque deserunt? Doloremque dolores recusandae laudantium? Praesentium,
-        impedit accusantium facere totam debitis aperiam dignissimos dolorum.
-        Consectetur exercitationem reiciendis hic ipsa necessitatibus rerum
-        aliquam architecto dolorum doloremque nam. Modi culpa numquam
-        consequuntur excepturi, neque veritatis accusamus explicabo temporibus
-        unde repellendus? Dignissimos, vel officiis cum autem exercitationem
-        dolorem aliquam, omnis assumenda sapiente unde eligendi illo quasi eaque
-        reprehenderit quia asperiores earum velit quo nisi itaque non. Veniam
-        deleniti perspiciatis excepturi maiores ratione eos ex at aut? Quam
-        cupiditate ab dolor quaerat explicabo temporibus culpa hic ex in minus
-        voluptas, eligendi praesentium dolorem odio ea consequuntur sunt tempore
-        nulla voluptatem corporis? Distinctio obcaecati asperiores amet! Rem,
-        dolore! Incidunt perspiciatis, hic necessitatibus quibusdam molestiae
-        molestias optio esse sapiente inventore nemo animi! Accusantium eaque,
-        aut error aperiam esse ex quo illo laboriosam quos! Voluptates itaque
-        cumque mollitia doloremque. Asperiores dicta, corrupti sed provident
-        sit, consequuntur repellendus quisquam quas incidunt repellat amet
-        voluptates eum ipsum vel, atque molestias doloremque officia quo. Id
-        consectetur at quos blanditiis mollitia itaque eveniet omnis non
-        voluptatem officiis culpa, autem, eligendi temporibus error ea. Autem,
-        praesentium reiciendis repudiandae unde asperiores cum dignissimos ut
-        perspiciatis culpa mollitia neque, fugiat dolor quas accusamus
-        voluptatem commodi? Excepturi repellendus totam sunt qui non sequi unde,
-        vero quidem possimus nisi rerum cumque, laboriosam dicta modi dolorum,
-        quisquam sit nobis quis odit dolorem ipsum perferendis ratione.
-        Voluptate praesentium vero possimus quo porro officiis voluptas qui
-        magnam ullam nihil expedita assumenda exercitationem, itaque, nemo minus
-        quis modi maiores vel magni. Obcaecati autem iste, quae corporis ipsum
-        deserunt quia recusandae consequuntur hic, voluptates sed nulla
-        doloribus rem itaque porro repellat culpa odit, nemo repudiandae!
+                Born and raised in Sri Lanka, I schooled at St. Josephs College,
+                after which I switched to Asian International School where I
+                completed my secondary and pre university qualifications.
       </p>
+              <p>
+                I actively participated in multiple sports such as basketball,
+                football, karate and track &#38; field events. I would choose
+                javelin throwing, followed by football and basketball, if I had
+                to rank them in order of my favorite.
+              </p>
+              <p>
+                While I was small, I had a keen interest in exploring, inventing
+                and creating, which lead me to develop an interest in mutiple
+                fields, including software development.
+              </p>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <p></p>
     </div>
 
     <div class="cover">
