c890d72f01af26ba22021c4493fd47d2258286de
user4302 <4ngelo@outlook.com>
2021-10-28 21:58:09 +0530

    added cover letter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -85,125 +85,52 @@
     <div class="cover">
       <h1>Cover Letter</h1>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        accusamus, dolore animi perspiciatis, ipsam rem nihil quae hic veritatis
-        dignissimos aperiam adipisci! Eum maxime, ipsum molestiae, vero ducimus
-        eius, nobis obcaecati non iure nostrum inventore. Autem cumque dolorum
-        inventore ut magni vero expedita repellendus alias iure impedit quam,
-        magnam quisquam quasi itaque. Velit ea nobis reprehenderit eius.
-        Repellat eveniet consectetur, harum sed distinctio enim officia, quidem
-        hic aspernatur doloremque repellendus facilis esse nulla voluptatum iste
-        deleniti numquam illo. Iure, cupiditate eligendi inventore, illum
-        repudiandae corporis veritatis dicta assumenda fugit, eos doloribus?
-        Culpa aut, praesentium quisquam accusamus laborum unde? Minima explicabo
-        error quas odit. Quam sunt officiis voluptatem sapiente, velit commodi
-        exercitationem libero nisi dolores similique veniam aspernatur facere
-        quos qui nesciunt soluta! Sequi, nulla architecto, modi dolores,
-        eligendi laboriosam accusamus magnam quaerat minus quos fugiat hic
-        dolorum asperiores. Dolorem nulla blanditiis veritatis, ab, laborum modi
-        labore dolores odio, at optio recusandae sed rem consequatur vitae
-        suscipit sint quae. Illum molestiae, non natus vitae voluptatum
-        explicabo nam veritatis obcaecati? Consequatur aut reiciendis quasi nam
-        debitis sed, ab architecto! Impedit magnam dolores iusto? Deleniti ut
-        asperiores quibusdam eligendi officia necessitatibus totam vel, autem in
-        iusto soluta fuga vitae distinctio natus voluptates culpa corporis
-        facilis accusantium? Explicabo, consequatur. Expedita, doloremque modi
-        cupiditate enim earum sapiente praesentium sunt, quos veniam repellat
-        nam beatae error illum voluptatem voluptatibus. Exercitationem fugiat in
-        earum perferendis tempore, quia officia magni, a laboriosam, fugit
-        cumque deserunt? Doloremque dolores recusandae laudantium? Praesentium,
-        impedit accusantium facere totam debitis aperiam dignissimos dolorum.
-        Consectetur exercitationem reiciendis hic ipsa necessitatibus rerum
-        aliquam architecto dolorum doloremque nam. Modi culpa numquam
-        consequuntur excepturi, neque veritatis accusamus explicabo temporibus
-        unde repellendus? Dignissimos, vel officiis cum autem exercitationem
-        dolorem aliquam, omnis assumenda sapiente unde eligendi illo quasi eaque
-        reprehenderit quia asperiores earum velit quo nisi itaque non. Veniam
-        deleniti perspiciatis excepturi maiores ratione eos ex at aut? Quam
-        cupiditate ab dolor quaerat explicabo temporibus culpa hic ex in minus
-        voluptas, eligendi praesentium dolorem odio ea consequuntur sunt tempore
-        nulla voluptatem corporis? Distinctio obcaecati asperiores amet! Rem,
-        dolore! Incidunt perspiciatis, hic necessitatibus quibusdam molestiae
-        molestias optio esse sapiente inventore nemo animi! Accusantium eaque,
-        aut error aperiam esse ex quo illo laboriosam quos! Voluptates itaque
-        cumque mollitia doloremque. Asperiores dicta, corrupti sed provident
-        sit, consequuntur repellendus quisquam quas incidunt repellat amet
-        voluptates eum ipsum vel, atque molestias doloremque officia quo. Id
-        consectetur at quos blanditiis mollitia itaque eveniet omnis non
-        voluptatem officiis culpa, autem, eligendi temporibus error ea. Autem,
-        praesentium reiciendis repudiandae unde asperiores cum dignissimos ut
-        perspiciatis culpa mollitia neque, fugiat dolor quas accusamus
-        voluptatem commodi? Excepturi repellendus totam sunt qui non sequi unde,
-        vero quidem possimus nisi rerum cumque, laboriosam dicta modi dolorum,
-        quisquam sit nobis quis odit dolorem ipsum perferendis ratione.
-        Voluptate praesentium vero possimus quo porro officiis voluptas qui
-        magnam ullam nihil expedita assumenda exercitationem, itaque, nemo minus
-        quis modi maiores vel magni. Obcaecati autem iste, quae corporis ipsum
-        deserunt quia recusandae consequuntur hic, voluptates sed nulla
-        doloribus rem itaque porro repellat culpa odit, nemo repudiandae!
+        My name is Angelo, I have recently completed my degree and am awaiting
+        graduation and I would like to help your company hit and exceed your
+        development goals as a software engineer. As someone who is attentive to
+        detail and goal driven, I thrive on completing my tasks and delivering
+        results beyond expectations. With a bachelor's degree in software
+        engineering and hands on experience in the industry, I am confident that
+        I will be an asset to your company.
+      </p>
+      <p>
+        I have worked with iTelaSoft for just over 1 year and one of my roles
+        was a fullstack web developer for multiple projects. I was tasked mainly
+        with creating the frontend according to the clients accepted design and
+        was required to make immediate changes upon the client's request which
+        enabled us to create and test our implementations against the client's
+        requirements as soon as possible and reach our goal of completion
+        faster.
+      </p>
+      <p>
+        In addition to fullstack web development, I was also tasked with IoT
+        development primarily in hardware development, and secondarily in
+        programming the hardware, I was additionally tasked with writing the
+        technical documentation for the IoT devices as well as the website
+        associated with the said devices. Managing all the tasks and
+        requirements in a timeline was also assigned to me for some projects, to
+        make sure that all the requirements were completed and were viewable to
+        the entire team as well as the client.
+      </p>
+      <p>
+        I am proficient in a range of skills such as web development, native
+        mobile application development, penetration testing, technical writing,
+        and image editing, and I wish to apply for a full-time software
+        engineering position and will be able to take on specialized tasks in
+        the future as I gauge my performance with time.
+      </p>
+      <p>
+        Thank you for your time and consideration. I am looking forward to
+        learning more about the Software Engineering position at your company. I
+        am excited about the opportunity to leverage my unique range of skills,
+        and I am eager to demonstrate my commitment to developing world-class
+        software solutions for your company.
       </p>
     </div>
 
     <div class="resume">
       <h1>Resume</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        accusamus, dolore animi perspiciatis, ipsam rem nihil quae hic veritatis
-        dignissimos aperiam adipisci! Eum maxime, ipsum molestiae, vero ducimus
-        eius, nobis obcaecati non iure nostrum inventore. Autem cumque dolorum
-        inventore ut magni vero expedita repellendus alias iure impedit quam,
-        magnam quisquam quasi itaque. Velit ea nobis reprehenderit eius.
-        Repellat eveniet consectetur, harum sed distinctio enim officia, quidem
-        hic aspernatur doloremque repellendus facilis esse nulla voluptatum iste
-        deleniti numquam illo. Iure, cupiditate eligendi inventore, illum
-        repudiandae corporis veritatis dicta assumenda fugit, eos doloribus?
-        Culpa aut, praesentium quisquam accusamus laborum unde? Minima explicabo
-        error quas odit. Quam sunt officiis voluptatem sapiente, velit commodi
-        exercitationem libero nisi dolores similique veniam aspernatur facere
-        quos qui nesciunt soluta! Sequi, nulla architecto, modi dolores,
-        eligendi laboriosam accusamus magnam quaerat minus quos fugiat hic
-        dolorum asperiores. Dolorem nulla blanditiis veritatis, ab, laborum modi
-        labore dolores odio, at optio recusandae sed rem consequatur vitae
-        suscipit sint quae. Illum molestiae, non natus vitae voluptatum
-        explicabo nam veritatis obcaecati? Consequatur aut reiciendis quasi nam
-        debitis sed, ab architecto! Impedit magnam dolores iusto? Deleniti ut
-        asperiores quibusdam eligendi officia necessitatibus totam vel, autem in
-        iusto soluta fuga vitae distinctio natus voluptates culpa corporis
-        facilis accusantium? Explicabo, consequatur. Expedita, doloremque modi
-        cupiditate enim earum sapiente praesentium sunt, quos veniam repellat
-        nam beatae error illum voluptatem voluptatibus. Exercitationem fugiat in
-        earum perferendis tempore, quia officia magni, a laboriosam, fugit
-        cumque deserunt? Doloremque dolores recusandae laudantium? Praesentium,
-        impedit accusantium facere totam debitis aperiam dignissimos dolorum.
-        Consectetur exercitationem reiciendis hic ipsa necessitatibus rerum
-        aliquam architecto dolorum doloremque nam. Modi culpa numquam
-        consequuntur excepturi, neque veritatis accusamus explicabo temporibus
-        unde repellendus? Dignissimos, vel officiis cum autem exercitationem
-        dolorem aliquam, omnis assumenda sapiente unde eligendi illo quasi eaque
-        reprehenderit quia asperiores earum velit quo nisi itaque non. Veniam
-        deleniti perspiciatis excepturi maiores ratione eos ex at aut? Quam
-        cupiditate ab dolor quaerat explicabo temporibus culpa hic ex in minus
-        voluptas, eligendi praesentium dolorem odio ea consequuntur sunt tempore
-        nulla voluptatem corporis? Distinctio obcaecati asperiores amet! Rem,
-        dolore! Incidunt perspiciatis, hic necessitatibus quibusdam molestiae
-        molestias optio esse sapiente inventore nemo animi! Accusantium eaque,
-        aut error aperiam esse ex quo illo laboriosam quos! Voluptates itaque
-        cumque mollitia doloremque. Asperiores dicta, corrupti sed provident
-        sit, consequuntur repellendus quisquam quas incidunt repellat amet
-        voluptates eum ipsum vel, atque molestias doloremque officia quo. Id
-        consectetur at quos blanditiis mollitia itaque eveniet omnis non
-        voluptatem officiis culpa, autem, eligendi temporibus error ea. Autem,
-        praesentium reiciendis repudiandae unde asperiores cum dignissimos ut
-        perspiciatis culpa mollitia neque, fugiat dolor quas accusamus
-        voluptatem commodi? Excepturi repellendus totam sunt qui non sequi unde,
-        vero quidem possimus nisi rerum cumque, laboriosam dicta modi dolorum,
-        quisquam sit nobis quis odit dolorem ipsum perferendis ratione.
-        Voluptate praesentium vero possimus quo porro officiis voluptas qui
-        magnam ullam nihil expedita assumenda exercitationem, itaque, nemo minus
-        quis modi maiores vel magni. Obcaecati autem iste, quae corporis ipsum
-        deserunt quia recusandae consequuntur hic, voluptates sed nulla
-        doloribus rem itaque porro repellat culpa odit, nemo repudiandae!
-      </p>
+      <p>resume stuff</p>
     </div>
   </div>
 </template>
