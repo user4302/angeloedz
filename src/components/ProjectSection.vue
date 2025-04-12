fd57516a32a6d6de8ab3cac6430f952a2a25d554
Angelo E De Zoysa <4ngelo@outlook.com>
2025-04-12 21:38:33 +0530

    add fade in for lazy load of project cards

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="section">
     <div class="section-buttons">
-      <button v-for="category in categories" :key="category" @click="filterCategory(category)">
+      <button
+        v-for="category in categories"
+        :key="category"
+        @click="filterCategory(category)"
+      >
         {{ category }}
       </button>
     </div>
@@ -11,6 +15,7 @@
         :key="index"
         :imageSrc="card.imageSrc"
         :iconClass="card.iconClass"
+        :class="{ 'fade-in': card.fadeIn }"
       />
     </div>
   </div>
@@ -18,20 +23,20 @@
 
 <script>
 import ProjectCard from './ProjectCard.vue';
-import baftaImage from '@/assets/images/projectCardThumbnails/bafta.jpg'; // Import the image
+import baftaImage from '@/assets/images/projectCardThumbnails/bafta.jpg';
 
 export default {
   name: 'ProjectSection',
   components: {
-    ProjectCard
+    ProjectCard,
   },
   data() {
     return {
       categories: ['Frontend', 'Backend', 'Scripting', 'Mobile'],
-      allCards: [], // This will be populated with card data
+      allCards: [],
       visibleCards: [],
       currentCategory: 'Frontend',
-      cardsPerLoad: 6
+      cardsPerLoad: 6,
     };
   },
   methods: {
@@ -40,67 +45,80 @@ export default {
       this.loadCards();
     },
     loadCards() {
-      // Filter and load cards based on the current category
-      const filteredCards = this.allCards.filter(card => card.category === this.currentCategory);
-      this.visibleCards = filteredCards.slice(0, this.cardsPerLoad);
+      const filteredCards = this.allCards.filter(
+        (card) => card.category === this.currentCategory
+      );
+      this.visibleCards = filteredCards
+        .slice(0, this.cardsPerLoad)
+        .map((card) => ({ ...card, fadeIn: true }));
     },
     handleScroll() {
-      const bottomOfWindow = window.innerHeight + window.scrollY >= document.documentElement.offsetHeight;
+      const bottomOfWindow =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.offsetHeight;
       if (bottomOfWindow) {
         this.loadMoreCards();
       }
     },
     loadMoreCards() {
-      const filteredCards = this.allCards.filter(card => card.category === this.currentCategory);
+      const filteredCards = this.allCards.filter(
+        (card) => card.category === this.currentCategory
+      );
       const currentLength = this.visibleCards.length;
-      const moreCards = filteredCards.slice(currentLength, currentLength + this.cardsPerLoad);
+      const moreCards = filteredCards
+        .slice(currentLength, currentLength + this.cardsPerLoad)
+        .map((card) => ({ ...card, fadeIn: false }));
       this.visibleCards = this.visibleCards.concat(moreCards);
-    }
+
+      // Trigger fade-in effect
+      setTimeout(() => {
+        moreCards.forEach((card) => (card.fadeIn = true));
+      }, 100); // Delay to allow CSS transition
+    },
   },
   mounted() {
     window.addEventListener('scroll', this.handleScroll);
-    // Initialize with some card data
     this.allCards = [
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Mobile' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Mobile' },
-    { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Mobile' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Frontend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Backend' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Scripting' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Mobile' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Mobile' },
+      { imageSrc: baftaImage, iconClass: 'fab fa-js', category: 'Mobile' },
       // Add more card data here
     ];
     this.loadCards();
   },
   beforeUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
@@ -129,15 +147,21 @@ export default {
 .card-container {
   display: flex;
   flex-wrap: wrap;
-  justify-content: center; /* Center the cards */
-  gap: 20px; /* Add space between cards */
-  max-width: 1000px; /* Optional: Limit the maximum width of the container */
-  margin: 0 auto; /* Center the container */
+  justify-content: center;
+  gap: 20px;
+  max-width: 1000px;
+  margin: 0 auto;
 }
 
 .project-card {
-  flex: 0 0 calc(33.33% - 20px); /* Ensure three cards per row with spacing */
-  max-width: 300px; /* Optional: Limit the maximum width of each card */
-  box-sizing: border-box; /* Include padding and border in the element's total width and height */
+  flex: 0 0 calc(33.33% - 20px);
+  max-width: 300px;
+  box-sizing: border-box;
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+
+.fade-in {
+  opacity: 1;
 }
 </style>
