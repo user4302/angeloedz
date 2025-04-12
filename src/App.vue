679867fd15b7a9c5b2f9db8a9e79ad705106a109
Angelo E De Zoysa <4ngelo@outlook.com>
2025-04-12 20:51:19 +0530

    add hero section remove helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HeroSection />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeroSection from './components/HeroSection.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeroSection
   }
 }
 </script>
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
