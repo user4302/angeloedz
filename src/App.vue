14a154b03f815b3cc08f66f99b942c772dccaf25
Angelo E De Zoysa <4ngelo@outlook.com>
2025-04-15 03:04:01 +0530

    undo dedicated download vc component, will be completed later

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,20 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app"></div>
+  <router-view />
 
-    <!-- Floating Download Icon -->
-    <a
-      href="/docs/ANGELO E DE ZOYSA.pdf"
-      class="floating-icon"
-      title="Download CV"
-      download
-    >
-      <span class="material-icons">description</span>
-    </a>
-
-    <!-- Floating Contact Button -->
-    <button class="floating-contact-icon" @click="openContactForm">
-      <span class="material-icons">contact_mail</span>
-    </button>
-
-    <!-- Contact Form Component -->
-    <ContactForm ref="contactForm" />
-  </div>
+  <a
+    href="/docs/ANGELO E DE ZOYSA.pdf"
+    description
+    class="floating-icon"
+    title="Download CV"
+  >
+    <span class="material-icons">description</span>
+  </a>
 </template>
 
 <script>
-import ContactForm from './components/ContactForm.vue';
-
 export default {
   name: 'App',
-  components: {
-    ContactForm,
-  },
-  methods: {
-    openContactForm() {
-      this.$refs.contactForm.openForm();
-    },
-  },
 };
 </script>
 
@@ -47,13 +27,13 @@ export default {
   position: relative;
 }
 
-.floating-icon,
-.floating-contact-icon {
+.floating-icon {
   position: fixed;
   bottom: 20px;
+  right: 20px;
   color: white;
   padding: 10px;
-  border-radius: 50%;
+  border-radius: 30%;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
   transition: background-color 0.3s ease;
   text-align: center;
@@ -61,16 +41,7 @@ export default {
   z-index: 1000;
 }
 
-.floating-icon {
-  right: 20px;
-}
-
-.floating-contact-icon {
-  right: 80px; /* Adjust position to avoid overlap with the download icon */
-}
-
-.floating-icon:hover,
-.floating-contact-icon:hover {
+.floating-icon:hover {
   background-color: white;
   color: black;
 }
