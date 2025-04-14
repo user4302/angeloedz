880054d2495a96c15e972274b877da932082b1ab
Angelo E De Zoysa <4ngelo@outlook.com>
2025-04-15 03:09:37 +0530

    fix a bad commit, add contact form

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div id="app"></div>
-  <router-view />
+  <div id="app">
+    <router-view />
 
-  <a
-    href="/docs/ANGELO E DE ZOYSA.pdf"
-    description
-    class="floating-icon"
-    title="Download CV"
-  >
-    <span class="material-icons">description</span>
-  </a>
+    <a
+      href="/docs/ANGELO E DE ZOYSA.pdf"
+      description
+      class="floating-icon"
+      title="Download CV"
+    >
+      <span class="material-icons">description</span>
+    </a>
+    <button class="floating-contact-icon" @click="openContactForm">
+      <span class="material-icons">contact_mail</span>
+    </button>
+
+    <ContactForm ref="contactForm" />
+  </div>
 </template>
 
 <script>
+import ContactForm from './components/ContactForm.vue';
+
 export default {
   name: 'App',
+  components: {
+    ContactForm,
+  },
+  methods: {
+    openContactForm() {
+      this.$refs.contactForm.openForm();
+    },
+  },
 };
 </script>
 
@@ -26,7 +42,24 @@ export default {
   text-align: center;
   position: relative;
 }
+.floating-contact-icon {
+  position: fixed;
+  bottom: 20px;
+  right: 80px; /* Adjust position to avoid overlap with the download icon */
+  color: white;
+  padding: 10px;
+  border-radius: 50%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s ease;
+  text-align: center;
+  cursor: pointer;
+  z-index: 1000;
+}
 
+.floating-contact-icon:hover {
+  background-color: white;
+  color: black;
+}
 .floating-icon {
   position: fixed;
   bottom: 20px;
