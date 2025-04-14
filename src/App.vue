47016987a076867b55bf879cf5d7170becd3d205
Angelo E De Zoysa <4ngelo@outlook.com>
2025-04-15 02:22:17 +0530

    add CV for download

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="app">
-  </div>
+  <div id="app"></div>
   <router-view />
+
+  <a
+    href="/docs/ANGELO E DE ZOYSA.pdf"
+    description
+    class="floating-icon"
+    title="Download CV"
+  >
+    <span class="material-icons">description</span>
+  </a>
 </template>
 
 <script>
@@ -16,5 +24,29 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  position: relative;
+}
+
+.floating-icon {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  color: white;
+  padding: 10px;
+  border-radius: 30%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s ease;
+  text-align: center;
+  cursor: pointer;
+  z-index: 1000;
+}
+
+.floating-icon:hover {
+  background-color: white;
+  color: black;
+}
+
+.material-icons {
+  font-size: 24px;
 }
 </style>
